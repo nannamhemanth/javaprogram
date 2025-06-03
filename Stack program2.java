36b6b50b9527fae365ef8c5481c85437180e958f
nannamhemanth <nannamhemanth@gmail.com>
2025-06-03 15:05:22 +0530

    Add files via upload

---
+++ Stack program2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args){
+        Stack s=new Stack<>();
+        s.push(1);
+        s.push(2);
+        System.out.println(s.peek());
+        System.out.println(s.pop());
+    }
+}
