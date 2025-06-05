eda415babe986f5663c400a28c04e0aa227412e5
nannamhemanth <nannamhemanth@gmail.com>
2025-06-05 15:04:33 +0530

    Add files via upload

---
+++ reverse list .java.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode past = null;  
+        ListNode curr = head;
+        ListNode next = null;
+
+        while (curr != null) {
+            next = curr.next;  
+            curr.next = past;  
+            past = curr;       
+            curr = next;
+        }
+
+        head = past;  
+        return head;
+    }
+}
