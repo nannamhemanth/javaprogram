f88bd93719fd88db85bb7c802c49b80a25ab6b43
nannamhemanth <nannamhemanth@gmail.com>
2025-06-06 19:44:27 +0530

    Add files via upload

---
+++ recurssion 2.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void add(int n)
+    {
+        if (n==10) return;
+        System.out.println(n);
+        add(n+1);
+    }
+    public static void main(String[] args){
+        add(1);
+    }
+}
