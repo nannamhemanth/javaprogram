a647a92b216ced1c60b8d731086c36bc9531b1eb
nannamhemanth <nannamhemanth@gmail.com>
2025-06-06 14:34:32 +0530

    Add files via upload

---
+++ static&non static.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void add()
+    {
+        int a=8;
+        int b=5;
+        int c=a+b;
+        System.out.println(c);
+    }
+    public static void main(String[] args){
+        add();
+    }
+}
