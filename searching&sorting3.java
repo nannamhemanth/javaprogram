21d353f494b678dbffe666c219d62517444db941
nannamhemanth <nannamhemanth@gmail.com>
2025-06-10 14:31:19 +0530

    Add files via upload

---
+++ searching&sorting3.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args){
+        int[] arr=new int[5];
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<arr.length-1;i++)
+        {
+            for(int j=0;j<arr.length;j++)
+            {
+                if (arr[i]>arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                   arr[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+    System.out.println("not found");
+		}
+    }
+}
